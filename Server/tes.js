7a5a860da6fda2d7a52563e5db3ffa20947024a9
mohammed elhachimi <57411879+mohammed873@users.noreply.github.com>
2021-04-16 11:18:47 +0000

    added test

---
+++ Server/tes.js
@@ -0,0 +1,32 @@
+// const app = require('../app') // Link to your server file
+// const supertest = require('supertest')
+// const request = supertest(app)
+// const db = require('../config/config.json')
+
+// // beforeAll(async () => {
+// //   await   db.sequelize.sync()
+// // })
+
+// // phone number must be valid
+// it('phone number must be valid', () => {
+//   expect('+212644444444').toMatch(
+//     /^(?:(?:(?:\+|00)212[\s]?(?:[\s]?\(0\)[\s]?)?)|0){1}(?:5[\s.-]?[2-3]|6[\s.-]?[13-9]){1}[0-9]{1}(?:[\s.-]?\d{2}){3}$/
+//   )
+// })
+
+// // registernumber must contain 9 charh
+// it('register number must be greater or equal to 9 degit', () => {
+//   expect('ookolkjnk').toMatch(/^[a-zA-Z]{9}$/)
+// })
+
+
+
+// // it('must return a valid token', async (done) => {
+// //   const res = await request.post('/login').send({
+// //     mail: 'tes@gmail.com',
+// //     password : 'test'
+// //   })
+// //   expect(res.status).toBe(200)
+// //   done()
+// // })
+
